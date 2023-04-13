af9f8991aabc7ad0bfd617890593027eb28055ac
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-04-12 20:07:16 -0500

    add async

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -11,19 +11,22 @@ Write a script that prints all characters of a Star Wars movie:
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
-request(url, function (error, response, body) {
+request(url, async function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
     const characters = JSON.parse(body).characters;
     for (const character of characters) {
-      request(character, function (error, response, body) {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log(JSON.parse(body).name);
-        }
+      const characterNames = await new Promise((resolve, reject) => {
+        request(character, function (error, characterNames, body) {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(JSON.parse(body).name);
+          }
+        });
       });
+      console.log(characterNames);
     }
   }
 });
