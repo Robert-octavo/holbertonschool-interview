4dac55334bf0d3431054b38d7dba330b0f246989
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-04-24 21:17:37 -0500

    Header file and big O

---
+++ 0x11-heap_sort/sort.h
@@ -0,0 +1,11 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+void heap_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
+void heapify(int *array, size_t size, size_t i, size_t size_orig);
+
+#endif /* SORT_H */
